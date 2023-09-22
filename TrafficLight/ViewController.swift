eea31a6031bd34ef80e735207587a0fc62480cfd
Viktor Smirnov <smirn04@yandex.ru>
2023-09-22 10:44:24 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Viktor Smirnov on 22.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var yellowLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func startButton() {
+    }
+    
+
+}
+
